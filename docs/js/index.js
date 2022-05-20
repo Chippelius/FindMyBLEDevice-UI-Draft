f5bbba4aacf7dc18f158b2956955ee370332868e
Leo Köberlein <leo@wolfgang-koeberlein.de>
2022-05-20 03:01:15 +0200

    Extended UI

--- docs/js/index.js
+++ docs/js/index.js
@@ -23,6 +23,25 @@
     }
   }
 
+  const SELECTED = "ble-button-1";
+  let selectedDevice = document.getElementById("selectedDevice");
+  let deviceButtons = document.getElementsByClassName("ble-button");
+  deviceButtons[0].style.fontWeight = "700";
+  deviceButtons[0].style.color = "rgb(20, 115, 165)";
+  selectedDevice.textContent = deviceButtons[0].textContent;
+  for(const button of deviceButtons) {
+    button.onclick = () => {
+      for(const otherButton of deviceButtons) {
+        otherButton.style.fontWeight = "";
+        otherButton.style.color = "";
+      }
+      button.style.fontWeight = "700";
+      button.style.color = "rgb(20, 115, 165)";
+      selectedDevice.textContent = button.textContent;
+      window.toggleOverlay();
+    }
+  }
+
 
 
 
@@ -44,16 +63,48 @@
 
   let helpContainer = document.getElementById("helpContainer");
   let helpVisible = false;
-  //helpContainer.style.visibility = "hidden";
-  //helpContainer.style.display = "block";
+  //helpContainer.style.display = "none";
 
   window.toggleHelp = function() {
     if(!helpVisible) {
-      helpContainer.style.display = "inherit";
+      helpContainer.style.display = "block";
       helpVisible = true;
     } else {
       helpContainer.style.display = "none";
       helpVisible = false;
+    }
+  }
+
+
+
+
+  let mains = Array.prototype.slice.call(document.getElementsByClassName("main"), 0).reverse();
+  let backButton = document.getElementById("backButton");
+  let switchContainer = document.getElementById("switchContainer");
+  let switchGPS = document.getElementById("switchGPS");
+  let switchBluetooth = document.getElementById("switchBluetooth");
+
+  window.setMain = function(i) {
+    for(let j=0; j<mains.length; ++j) {
+      mains[j].style.left = "" + ((j - i) * 100) + "%";
+    }
+    if(i === 0) {
+      backButton.style.width = "1px";
+      backButton.style.paddingLeft = "0px";
+      //backButton.style.paddingRight = "0px";
+      switchContainer.style.top = "-63px";
+    } else {
+      backButton.style.width = "55px";
+      backButton.style.paddingLeft = "20px";
+      //backButton.style.paddingRight = "15px";
+      switchContainer.style.top = "0px";
+      if(i === 1) {
+        switchGPS.style.backgroundColor = 'rgb(73, 80, 88)';
+        switchBluetooth.style.backgroundColor = 'rgb(46, 52, 57)';
+      } else {
+        switchGPS.style.backgroundColor = 'rgb(46, 52, 57)';
+        switchBluetooth.style.backgroundColor = 'rgb(73, 80, 88)';
+      }
     }
   }
 
